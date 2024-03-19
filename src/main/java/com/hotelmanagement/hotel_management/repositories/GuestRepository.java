3e076d55e8aa3115aea069a37a651be241d73d2f
Sofiia Maliarenko <maliarenko.sofiia@gmail.com>
2024-03-19 22:33:10 +0200

    Update the guests.html

--- src/main/java/com/hotelmanagement/hotel_management/repositories/GuestRepository.java
+++ src/main/java/com/hotelmanagement/hotel_management/repositories/GuestRepository.java
@@ -1,13 +1,13 @@
 package com.hotelmanagement.hotel_management.repositories;
 
 import com.hotelmanagement.hotel_management.data.Guest;
-import com.hotelmanagement.hotel_management.data.GuestInfo;
+//import com.hotelmanagement.hotel_management.data.GuestInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface GuestRepository extends JpaRepository<Guest, Integer> {
-    List<GuestInfo> findByIdNotNull();
+//    List<GuestInfo> findByIdNotNull();
     List<Guest> findByFirstNameContainingOrLastNameContainingOrPassportInfoContainingOrContactNumberContaining(String firstName, String lastName, String passportInfo, String contactNumber);
 
 }
